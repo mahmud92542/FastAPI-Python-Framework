d8ce13e804c0fb1be6dc96a2613bc2f8528e5d0e
mahmud92542 <mhasan92542@gmail.com>
2021-06-27 13:35:44 +0600

    add some new lines and comment

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/') #path operation decorator #operation #path
+#path operation function
 def index():
     return {'data': {
-        'name': 'Mahmud!'
+        'name': 'blog list!'
     }
 }
     
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+@app.get('/blog/{id}') #path operation decorator #operation #path
+#path operation function
+def about(id):
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{
+        '2','3'
+    }}
+    
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
